cdf4b8b7ab16ba2ea1f52b89b25f6ac80f27e631
kalechao87 <kalechao@outlook.com>
2021-03-01 16:04:55 +0800

    color generator

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+// import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, index, weight, hexColor }) => {
+  const [alert, setAlert] = useState(false);
 
-export default SingleColor
+  const bgc = rgb.join(',');
+  const hexValue = `#${hexColor.toUpperCase()}`;
+
+  const handleCopyClick = () => {
+    if (navigator.clipboard) {
+      console.log('Clipboard API available');
+      window.alert('Clipboard API available');
+      setAlert(true);
+      navigator.clipboard.writeText(hexValue);
+    } else {
+      window.alert(navigator.clipboard, 'Clipboard API not available');
+    }
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bgc})` }}
+      onClick={handleCopyClick}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
